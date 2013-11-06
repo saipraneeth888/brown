4231354070cbd61900c8cabfd315d0c0573fb427
TMorreale0 <ThomasMorreale@Thomass-MacBook-Air.local>
2013-11-06 16:40:54 -0600

    Changes

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-$("#addbutton").click(function () {
+window.onload = setStage;
 
-    //simple image
-    var image = new Image();
-    image.onload = function () {
-        var image1 = new Kinetic.Image({
-            x: 0,
-            y: 0,
-            width: 300,
-            height: 300,
-            draggable: true,
-            image: image
-        });
-        layer.add(image1);
-        layer.draw();
-    }
-    image.src = "icon01.svg";
+function setStage() {
+var stage = new Kinetic.Stage({
+    container: 'container',
+    width: 300,
+    height: 300
 });
+var layer = new Kinetic.Layer();
+stage.add(layer);
 
 
+var image = new Image();
+image.onload = function () {
+
+    var image1 = new Kinetic.Image({
+        x: 0,
+        y: 0,
+        width: 300,
+        height: 300,
+        image: image,
+    });
+    layer.add(image1);
+
+    layer.draw();
+
+}
+image.src = "https://dl.dropboxusercontent.com/u/139992952/stackoverflow/KoolAid
+Man.png";
+
+$("#body01").click(function () {
+
+        var image = new Image();
+        image.onload = function () {
+        var image2 = new Kinetic.Image({
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 50,
+        image: image,
+        draggable: true
+        });
+        layer.add(image2);
+        layer.draw();
+} 
+image.src = "img/pic/body01.svg";
+/*
 function drawImage(imageObj) {
 	var stage = new Kinetic.Stage({
 	  container: "container",
@@ -51,4 +78,4 @@ function drawImage(imageObj) {
 	imageObj.onload = function() {
 	  drawImage(this);
 	};
-	imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+	imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';*/
