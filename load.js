bc7a668aede414b26ba5a5ac50049f8092084932
TMorreale0 <ThomasMorreale@Thomass-MacBook-Air.local>
2013-11-07 20:27:06 -0600

    Added Text adding
    Work in progress but works

--- load.js
+++ load.js
@@ -27,6 +27,27 @@ image.onload = function () {
 }
 image.src = "https://dl.dropboxusercontent.com/u/139992952/stackoverflow/KoolAidMan.png";
 */
+$("#addbutton").click(function () {
+    // simple label
+    var label = new Kinetic.Label({
+        x: 20,
+        y: 20,
+        draggable: true
+    });
+    label.add(new Kinetic.Tag({
+        fill: 'green'
+    }));
+    label.add(new Kinetic.Text({
+        text: $("#newtext").val(),
+        fontFamily: 'Verdana',
+        fontSize: 18,
+        padding: 10,
+        fill: 'white'
+    }));
+    layer.add(label);
+    layer.draw();
+});
+
 $("#body01").click(function () {
 
         var image = new Image();
