59a4fd5185d1f9356fa6219afaf6811994ff5d2d
TMorreale0 <ThomasMorreale@gmail.com>
2013-11-14 17:12:48 -0600

    Changed index.php so it runs php inside of index.html

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
-$response=file_get_contents("index.html");
-echo $response;
+//$response=file_get_contents("index.html");
+//echo $response;
+
+include('index.html');
 ?>
