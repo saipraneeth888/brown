09c4084136ba78d0aee9e2c392ee1c8cd762ab9b
Heather-Zhang <yangzhang22@yahoo.com>
2013-11-19 21:02:21 -0600

    save image done

--- js/load2.js
+++ js/load2.js
@@ -81,6 +81,7 @@ $("#changebackground").click(function () {
 
 
 
+
 $("#addbutton").click(function () {
     // simple label
     var label = new Kinetic.Label({
@@ -388,9 +389,20 @@ layer.on('dbltap', function(evt) {
         layer.draw();  
     }
 });
+
+
 document.getElementById('save').addEventListener('click', function() {
+        /*
+         * since the stage toDataURL() method is asynchronous, we need
+         * to provide a callback
+         */
         stage.toDataURL({
           callback: function(dataUrl) {
+            /*
+             * here you can do anything you like with the data url.
+             * In this tutorial we'll just open the url with the browser
+             * so that you can see the result as an image
+             */
             //window.open(dataUrl);
             //alert(dataUrl+"clicked");
             $.ajax({
@@ -404,5 +416,8 @@ document.getElementById('save').addEventListener('click', function() {
                 });
           }
         });
-    }, false);
+        
+      }, false);
+ 
+ 
 }
