40eaff89c593a53d42f1ae320593806069cb83e7
TMorreale0 <ThomasMorreale@Thomass-MacBook-Air.local>
2013-11-22 14:12:46 -0600

    Little change

--- Test/load.js
+++ Test/load.js
@@ -402,8 +402,8 @@ document.getElementById('save').addEventListener('click', function() {
                 }).done(function(o) {
                 console.log('saved');
                         }).done(function( data ) {
-                                alert('HI! ' + data);
-				//window.location.herf = "http://story-boarder.herokuapp.com/" + data;
+                                //alert('HI! ' + data);
+				window.location.herf = "http://story-boarder.herokuapp.com/Test/" + data;
                                 });
           }
         });
